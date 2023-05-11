97edb75afecc72b08b0bd56d8faa74dfe54b7513
kyungraekim <krkim741@gmail.com>
2023-05-12 02:46:08 +0900

    Integrate bottom navigation in MainActivity

--- app/src/main/java/com/example/scouter/MainActivity.java
+++ app/src/main/java/com/example/scouter/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.scouter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(
+                this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(
+                (BottomNavigationView) findViewById(R.id.nav_bottom_view), navController);
     }
 }
