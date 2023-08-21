d1d9c321602c091b2dc0ecbd2a40ff2f8bebdd42
kyungraekim <krkim741@gmail.com>
2023-08-21 22:54:10 +0900

    Fix BLE permissions

--- scan/src/main/java/com/example/scouter/scan/Permission.java
+++ scan/src/main/java/com/example/scouter/scan/Permission.java
@@ -10,12 +10,16 @@ public class Permission {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             BLUETOOTH_LOW_ENERGY = new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
                     Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_SCAN,
             };
         } else {
             BLUETOOTH_LOW_ENERGY = new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
             };
         }
     }
